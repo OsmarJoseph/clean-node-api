df3a067df530d2f44abbfdbad983ec64ff57670f
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-26 00:39:24 -0300

    feat: ensure Account Mongo Repository  return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,11 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (account: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(account)
+    return MongoHelper.map(result.ops[0])
+  }
+}

c7a861f268cf250376f8ea752764779ecd5fb3d9
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-28 12:56:47 -0300

    feat: ensure enable CORS

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
 import { Express } from 'express'
 import { bodyParser } from './middlewares/body-parser/body-parser'
+import { cors } from './middlewares/cors/cors'
 export function setupMiddlewares (app: Express): void {
   app.use(bodyParser)
+  app.use(cors)
 }

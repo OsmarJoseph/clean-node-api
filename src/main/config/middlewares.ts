d1a977b6d53caaff91c7bf2414587aacb9076064
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-28 13:06:18 -0300

    feat: ensure content type return default type as json

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,9 @@
 import { Express } from 'express'
 import { bodyParser } from './middlewares/body-parser/body-parser'
 import { cors } from './middlewares/cors/cors'
+import { contentType } from './middlewares/content-type/content-type'
 export function setupMiddlewares (app: Express): void {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }

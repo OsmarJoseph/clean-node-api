4da09d3af4b4b177be8f68a46c404799557b240a
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-28 13:12:54 -0300

    refactor: move middlewares interfaces to a single file and move middlewares folder to the main level

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,6 @@
 import { Express } from 'express'
-import { bodyParser } from './middlewares/body-parser/body-parser'
-import { cors } from './middlewares/cors/cors'
-import { contentType } from './middlewares/content-type/content-type'
+import { bodyParser , cors , contentType } from '../middlewares'
+
 export function setupMiddlewares (app: Express): void {
   app.use(bodyParser)
   app.use(cors)

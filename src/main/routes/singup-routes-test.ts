955721e5e974a3a84ac67b4f26e2b6583cc8f299
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-08-29 01:10:55 -0300

    refactor: connect to mongodb before tests

--- src/main/routes/singup-routes-test.ts
+++ src/main/routes/singup-routes-test.ts
@@ -1,15 +1,29 @@
 import request from 'supertest'
 import { app } from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes',() => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on success',async () => {
     await request(app).post('/signup')
-      // .send({
-      //   name: 'Osmar',
-      //   email: 'osmar@email.com',
-      //   password: '123',
-      //   passwordConfirmation: '123'
-      // })
+      .send({
+        name: 'Osmar',
+        email: 'osmar@email.com',
+        password: '123',
+        passwordConfirmation: '123'
+      })
       .expect(200)
   })
 })

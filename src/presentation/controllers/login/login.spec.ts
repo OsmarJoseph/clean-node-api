79d313c561d0b3fde536b630f8b66a9ddf40550b
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-02 13:13:12 -0300

    refactor: move sut creation to a object

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,8 +3,15 @@ import { HttpRequest } from '../../protocols'
 import { badRequest } from '../../helpers/http-helper'
 import { MissingParamError } from '../../errors'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
 }
 
 const makeMockRequest = (): HttpRequest => ({
@@ -15,14 +22,14 @@ const makeMockRequest = (): HttpRequest => ({
 })
 describe('Login Controller',() => {
   test('Should return statusCode 400 and Missing Param Error if no email is provided ', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = makeMockRequest()
     delete httpRequest.body.email
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('Should return statusCode 400 and Missing Param Error if no password is provided ', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = makeMockRequest()
     delete httpRequest.body.password
     const httpResponse = await sut.handle(httpRequest)

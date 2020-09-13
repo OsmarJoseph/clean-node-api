573e261c092eb8c064d950ed532327d2e9faef85
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-13 13:23:30 -0300

    test: ensure Jwt Adapter call sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import { JwtAdapter } from './jwt-adapter'
+const secretKey = 'secret'
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter(secretKey)
+}
+describe('Jwt Adapter', () => {
+  test('Should call sign with correct values',async () => {
+    const sut = makeSut()
+    const signSpy = jest.spyOn(jwt,'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' },'secret')
+  })
+})

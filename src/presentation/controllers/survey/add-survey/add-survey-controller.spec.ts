d66997215689fe2d716c954c6b0e4b9356f3f2ac
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-15 14:14:20 -0300

    test: return 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddSurvey, AddSurveyModel } from './add-survey-protocols'
 import { AddSurveyController } from './add-survey-controller'
-import { badRequest, serverErrorResponse } from '../../../helpers/http/http-helper'
+import { badRequest, serverErrorResponse,noContentResponse } from '../../../helpers/http/http-helper'
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
@@ -69,5 +69,10 @@ describe('Add Survey Controller', () => {
     jest.spyOn(addSurveyStub,'add').mockReturnValueOnce(new Promise((resolve,reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeHttpRquest())
     expect(httpResponse).toEqual(serverErrorResponse(new Error()))
+  })
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeHttpRquest())
+    expect(httpResponse).toEqual(noContentResponse())
   })
 })

3745c46fe35b5f3bfff411d802ae781a0d8bbe38
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-15 21:40:32 -0300

    test: ensure SurveyMongoRepository create a survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,36 @@
+import { Collection } from 'mongodb'
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+let surveyCollection: Collection
+const makeSurveyData = (): AddSurveyModel => (
+  {
+    question: 'any_question',
+    possibleAnswers: [{
+      image: 'any_image',
+      answer: 'any_answer'
+    }]
+  }
+)
+
+const makeSut = (): SurveyMongoRepository => new SurveyMongoRepository()
+describe('Survey Mongo Repository',() => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+  test('Should create a survey on success', async () => {
+    const sut = makeSut()
+    await sut.add(makeSurveyData())
+    const savedSurvey = await surveyCollection.findOne({})
+    expect(savedSurvey).toBeTruthy()
+  })
+})

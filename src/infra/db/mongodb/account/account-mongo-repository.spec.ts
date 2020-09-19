8627b1f00a821cc75ea458ee5e007ed1d9f023c4
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-19 12:43:52 -0300

    fix: ensure AccountMongoRepository allows admin access any role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -16,7 +16,7 @@ const makeMockAccount = async (withAccessToken?: boolean,withRole?: boolean): Pr
     newAccount.accessToken = 'any_token'
   }
   if (withRole) {
-    newAccount.role = 'any_role'
+    newAccount.role = 'admin'
   }
   const opResult = await accountCollection.insertOne(newAccount)
   return MongoHelper.map(opResult.ops[0])
@@ -85,10 +85,27 @@ describe('Account Mongo Repository',() => {
       expect(account.email).toBe('any_email@mail.com')
       expect(account.password).toBe('any_password')
     })
-    test('Should return an account on loadByToken with role',async () => {
+    test('Should return an account on loadByToken with role admin role',async () => {
       const sut = makeSut()
       await makeMockAccount(true,true)
       const account = await sut.loadByToken('any_token','any_role')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+    test('Should return null on loadByToken with invalid role',async () => {
+      const sut = makeSut()
+      await makeMockAccount(true,false)
+      const account = await sut.loadByToken('any_token','admin')
+      expect(account).toBeFalsy()
+    })
+
+    test('Should return an account on loadByToken if user is admin',async () => {
+      const sut = makeSut()
+      await makeMockAccount(true,true)
+      const account = await sut.loadByToken('any_token')
       expect(account).toBeTruthy()
       expect(account.id).toBeTruthy()
       expect(account.name).toBe('any_name')
--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -25,7 +25,7 @@ export class AccountMongoRepository implements AddAccountRepository,LoadAccountB
 
   async loadByToken (token: string,role?: string): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountCollection.findOne({ accessToken: token, role })
+    const account = await accountCollection.findOne({ accessToken: token, $or: [{ role },{ role: 'admin' }] })
     return MongoHelper.map(account)
   }
 }

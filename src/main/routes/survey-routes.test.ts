3289c55d6b29d5ee28a7abd195b935724288d982
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-19 00:52:54 -0300

    test: ensure add survey route return 204 with valid accessToken

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -2,7 +2,11 @@ import request from 'supertest'
 import { app } from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { AddSurveyModel } from '../../domain/usecases/add-survey'
+import { AccountModel } from '../../domain/models/account'
+import { AddAccountModel } from '../../domain/usecases/add-account'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import { env } from '../../main/config/env'
 const makeSurveyData = (): AddSurveyModel => (
   {
     question: 'Question 1',
@@ -12,7 +16,26 @@ const makeSurveyData = (): AddSurveyModel => (
     }]
   }
 )
+const makeAccountParams = (): AddAccountModel => ({
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
 let surveyCollection: Collection
+let accountCollection: Collection
+const makeMockAccount = async (): Promise<AccountModel> => {
+  const newAccount = makeAccountParams() as AccountModel
+  const opResult = await accountCollection.insertOne(newAccount)
+  return MongoHelper.map(opResult.ops[0])
+}
+const addValidAccessToAccount = async (account: AccountModel,withRole: boolean): Promise<string> => {
+  const accessToken = sign({ id: account.id }, env.jwtSecret)
+  const role = withRole ? 'admin' : ''
+  await accountCollection.updateOne({ _id: account.id },{ $set: { accessToken,role } })
+
+  return accessToken
+}
 describe('Survey Routes',() => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -25,6 +48,8 @@ describe('Survey Routes',() => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
   describe('POST /surveys',() => {
     test('Should return 403 on add survey without accessToken',async () => {
@@ -33,6 +58,15 @@ describe('Survey Routes',() => {
           makeSurveyData()
         )
         .expect(403)
+    })
+    test('Should return 204 with valid accessToken',async () => {
+      const mockAccount = await makeMockAccount()
+      const usedAccessToken = await addValidAccessToAccount(mockAccount,true)
+      await request(app).post('/api/surveys').set('x-access-token',usedAccessToken)
+        .send(
+          makeSurveyData()
+        )
+        .expect(204)
     })
   })
 })

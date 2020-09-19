a2d342d156c4e3d806bee583932a97833589087a
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-19 00:24:34 -0300

    feat: ensure add survey route returns 403 if no accessToken is provided

---
+++ src/main/factories/usecases/account/load-account-by-token/db-load-account-by-token-factory.ts
@@ -0,0 +1,10 @@
+import { DbLoadAccountByToken } from '../../../../../data/usecases/load-account-by-token/db-load-account-by-token'
+import { AccountMongoRepository } from '../../../../../infra/db/mongodb/account/account-mongo-repository'
+import { JwtAdapter } from '../../../../../infra/criptography/jwt-adapter/jwt-adapter'
+import { env } from '../../../../config/env'
+
+export const makeLoadAccountByToken = (): DbLoadAccountByToken => {
+  const jwtAdapter = new JwtAdapter(env.jwtSecret)
+  const accountMongoRepository = new AccountMongoRepository()
+  return new DbLoadAccountByToken(jwtAdapter,accountMongoRepository)
+}
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,7 +2,10 @@ import { Router } from 'express'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decorator-factory'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
+import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 
 export default (router: Router): void => {
-  router.post('/surveys', adaptRoute(makeLogControllerDecorator(makeAddSurveyController())))
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.post('/surveys',adminAuth, adaptRoute(makeLogControllerDecorator(makeAddSurveyController())))
 }

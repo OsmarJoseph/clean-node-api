20ad572b29154e6d27375f5c48890391efbd436f
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-20 12:33:22 -0300

    refactor: move auth middlewares to a factory method

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -2,14 +2,11 @@ import { Router } from 'express'
 import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeLogControllerDecorator } from '../factories/decorators/log-controller-decorator-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
-import { adaptMiddleware } from '../adapters/express/express-middleware-adapter'
 import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { normalAuth } from '../middlewares/normal-auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const normalAuth = adaptMiddleware(makeAuthMiddleware())
-
   router.post('/surveys',adminAuth, adaptRoute(makeLogControllerDecorator(makeAddSurveyController())))
   router.get('/surveys',normalAuth, adaptRoute(makeLogControllerDecorator(makeLoadSurveysController())))
 }

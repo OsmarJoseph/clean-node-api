13d5a5c518a6ee5964cb78897b06959b40be3640
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-21 20:20:02 -0300

    fix: change add to save on SaveSurveyResult

--- src/domain/usecases/save-survey-result.ts
+++ src/domain/usecases/save-survey-result.ts
@@ -3,5 +3,5 @@ import { SurveyResultModel } from '@/domain/models/survey-result'
 export type SaveSurveyResultModel = Omit<SurveyResultModel,'id'>
 
 export interface SaveSurveyResult {
-  add (surveyData: SaveSurveyResultModel): Promise<SurveyResultModel>
+  save (surveyData: SaveSurveyResultModel): Promise<SurveyResultModel>
 }

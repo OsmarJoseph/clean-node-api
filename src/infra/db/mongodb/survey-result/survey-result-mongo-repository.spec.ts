7f912a1d10e360114624bf4893bf06f6753e27af
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-21 23:06:48 -0300

    test: ensure SurveyResultMongoRepository update a survey result if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,8 +1,10 @@
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 import { SurveyModel } from '@/domain/models/survey'
 import { AccountModel } from '@/domain/models/account'
+import { SurveyResultModel } from '@/domain/models/survey-result'
 import { AddSurveyModel } from '@/domain/usecases/add-survey'
 import { AddAccountModel } from '@/domain/usecases/add-account'
+import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
 
@@ -16,6 +18,10 @@ const makeSurveyData = (): AddSurveyModel => (
     possibleAnswers: [{
       image: 'any_image',
       answer: 'any_answer'
+    },
+    {
+      image: 'other_image',
+      answer: 'other_answer'
     }],
     date: new Date()
   }
@@ -35,6 +41,11 @@ const insertSurveyOnDatabase = async (): Promise<SurveyModel> => {
 const insertAccountOnDatabase = async (): Promise<AccountModel> => {
   const account = await accountCollection.insertOne(makeAccountData())
   return MongoHelper.map(account.ops[0])
+}
+
+const insertSurveyResultOnDatabase = async (surveyResultData: SaveSurveyResultModel): Promise<SurveyResultModel> => {
+  const savedSurveyResult = await surveyResultCollection.insertOne(surveyResultData)
+  return MongoHelper.map(savedSurveyResult.ops[0])
 }
 
 const makeSut = (): SurveyResultMongoRepository => new SurveyResultMongoRepository()
@@ -72,6 +83,26 @@ describe('SurveyResultMongoRespository',() => {
       const savedSurveyResult = await sut.save({ ...surveyResultParams })
       expect(savedSurveyResult.id).toBeTruthy()
       expect(savedSurveyResult).toEqual({ ...surveyResultParams,id: savedSurveyResult.id })
+    })
+    test('Should update a survey result if its not new', async () => {
+      const survey = await insertSurveyOnDatabase()
+      const account = await insertAccountOnDatabase()
+      const sut = makeSut()
+      const surveyResultParams = {
+        surveyId: survey.id,
+        accountId: account.id,
+        answer: survey.possibleAnswers[0].answer,
+        date: new Date()
+      }
+      const updatedAnswer = survey.possibleAnswers[1].answer
+      const savedMockSurveyResult = await insertSurveyResultOnDatabase({ ...surveyResultParams })
+      const updatedSurveyResult = await sut.save(
+        { ...surveyResultParams,answer: updatedAnswer }
+      )
+      expect(updatedSurveyResult.id).toEqual(savedMockSurveyResult.id)
+      expect(updatedSurveyResult).toEqual(
+        { ...surveyResultParams,id: updatedSurveyResult.id,answer: updatedAnswer }
+      )
     })
   })
 })

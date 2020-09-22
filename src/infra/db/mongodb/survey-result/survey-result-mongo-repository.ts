25ecff8f260b85d99fea01665042b65d8697657e
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-21 23:05:42 -0300

    feat: ensure SurveyResultMongoRepository update a survey result if its not new

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -2,11 +2,25 @@ import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey/save-survey-result-repository'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection, WithId } from 'mongodb'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
   async save (surveyResultData: SaveSurveyResultModel): Promise<SurveyResultModel> {
-    const surveyResultsCollection = await MongoHelper.getCollection('surveyResults')
-    const savedSurveyResult = await surveyResultsCollection.insertOne(surveyResultData)
-    return MongoHelper.map(savedSurveyResult.ops[0])
+    const surveyResultsCollection = await MongoHelper.getCollection('surveyResults') as Collection<WithId<SaveSurveyResultModel>>
+    const { surveyId,accountId,answer,date } = surveyResultData
+    const savedSurveyResult = await surveyResultsCollection.findOneAndUpdate(
+      {
+        surveyId,
+        accountId
+      },{
+        $set: {
+          answer,
+          date
+        }
+      },{
+        upsert: true,
+        returnOriginal: false
+      })
+    return savedSurveyResult.value && MongoHelper.map(savedSurveyResult.value)
   }
 }

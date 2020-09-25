c827a8e0f05ec10abf0ff8e50234052e3a1ad2ea
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-24 22:12:31 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,24 @@
+import { loginPath } from './paths'
+import { accountSchema, loginParamsSchema } from './schemas'
+
 export const docsConfig = {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API para realizar enquetes',
     version: '1.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    'login-params': loginParamsSchema
   }
 }

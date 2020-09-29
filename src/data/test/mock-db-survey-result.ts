4753778be8eaad9912b5cde66a4581e03324d0f6
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-28 22:21:14 -0300

    feat: ensure DBLoadSurveyResult use case call LoadSurveyResultRepository with correct surveyId

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -1,6 +1,7 @@
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { makeMockSurveyResultModel } from '@/domain/test'
 
 export const makeMockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
@@ -10,4 +11,13 @@ export const makeMockSaveSurveyResultRepository = (): SaveSurveyResultRepository
     }
   }
   return new SaveSurveyResultRepositoryStub()
+}
+
+export const makeMockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return makeMockSurveyResultModel()
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
 }

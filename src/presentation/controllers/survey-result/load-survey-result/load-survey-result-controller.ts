24cfeca49dbea002977377b11e47cec09c0deefb
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-29 21:13:41 -0300

    feat: ensure LoadSurveyResultController call loadSurveyById with correct surveyId

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -0,0 +1,14 @@
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { LoadSurveyById } from './load-survey-result-protocols'
+
+export class LoadSurveyResultController implements Controller {
+  constructor (
+    private readonly loadSurveyById: LoadSurveyById
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { params: { surveyId } } = httpRequest
+    await this.loadSurveyById.loadById(surveyId)
+    return null
+  }
+}

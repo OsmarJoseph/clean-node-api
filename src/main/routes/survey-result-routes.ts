0227317d0465093ae8e62ca74e719e93e72bf750
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-29 21:53:53 -0300

    feat: ensure LoadSurveyResultController return 403 on load survey result without accessToken

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -1,8 +1,10 @@
 import { makeSaveSurveyResultController } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { makeLoadSurveyResultController } from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
 import { normalAuth } from '@/main/middlewares'
 import { adaptRoute } from '@/main/adapters/express/express-route-adapter'
 import { Router } from 'express'
 
 export default (router: Router): void => {
   router.put('/surveys/:surveyId/results',normalAuth, adaptRoute(makeSaveSurveyResultController()))
+  router.get('/surveys/:surveyId/results',normalAuth, adaptRoute(makeLoadSurveyResultController()))
 }

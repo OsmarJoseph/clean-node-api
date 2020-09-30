ba5d241b0e99de3deb08271fcb800ef10ed08871
Osmar Joseph <osmarjoseph@MacBook-de-Kai.local>
2020-09-29 22:06:22 -0300

    docs: add load survey result path

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -43,5 +43,41 @@ export const surveyResultPath = {
         $ref: '#/components/serverErrorComponent'
       }
     }
+  },
+  get: {
+    tags: ['Enquete'],
+    summary: 'API para consultar o resultado de uma enquete',
+    security: [{
+      apiKeyAuthSchema: []
+    }],
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResultSchema'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidenRequestComponent'
+      },
+      404: {
+        $ref: '#/components/notFoundComponent'
+      },
+      500: {
+        $ref: '#/components/serverErrorComponent'
+      }
+    }
   }
 }

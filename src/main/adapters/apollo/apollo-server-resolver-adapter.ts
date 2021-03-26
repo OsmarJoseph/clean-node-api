619340d4543c48dccf2c015cd1ce289393d7bddf
Osmar <osmarjoseph2013@hotmail.com>
2021-03-26 18:45:27 -0300

    feat: add status code error on exceptions

--- src/main/adapters/apollo/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo/apollo-server-resolver-adapter.ts
@@ -1,6 +1,16 @@
 import { Controller } from '@/presentation/protocols'
+import { UserInputError, AuthenticationError, ApolloError, ForbiddenError } from 'apollo-server-express'
 
 export const adaptResolver = async (controller: Controller,args: unknown): Promise<unknown> => {
   const httpResponse = await controller.handle(args)
-  return httpResponse.body
+  const { statusCode, body } = httpResponse
+  const { message: errorMessage } = body
+
+  switch (statusCode) {
+    case 200: case 204: return body
+    case 400: throw new UserInputError(errorMessage)
+    case 401: throw new AuthenticationError(errorMessage)
+    case 403: throw new ForbiddenError(errorMessage)
+    default: throw new ApolloError(errorMessage)
+  }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -2,12 +2,41 @@ import { resolvers } from '@/main/graphql/resolvers'
 import { typeDefs } from '@/main/graphql/type-defs'
 
 import { ApolloServer } from 'apollo-server-express'
+import { GraphQLResponse } from 'apollo-server-types'
 import { Express } from 'express'
+import { GraphQLError } from 'graphql'
 
 export const setupApolloServer = (app: Express): void => {
   const server = new ApolloServer({
     resolvers,
-    typeDefs
+    typeDefs,
+    plugins: [{
+      requestDidStart: () => ({
+        willSendResponse: ({ response,errors }) => handleErrors(response, errors)
+      })
+    }]
   })
   server.applyMiddleware({ app })
+}
+
+const errorsMap = new Map([
+  ['UserInputError',{ statusCode: 400 }],
+  ['AuthenticationError',{ statusCode: 401 }],
+  ['ForbiddenError',{ statusCode: 403 }]
+])
+
+const handleErrors = (response: GraphQLResponse, errors: readonly GraphQLError[]): void => {
+  errors?.forEach(error => {
+    response.data = undefined
+
+    for (const [errorName, { statusCode }] of errorsMap) {
+      if (checkError(error,errorName)) {
+        response.http.status = statusCode
+      }
+    }
+  })
+}
+
+const checkError = (error: GraphQLError,errorName: string): boolean => {
+  return [error.name,error.originalError?.name].includes(errorName)
 }

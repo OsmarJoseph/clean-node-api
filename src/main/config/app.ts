9bc5569182d5dacadd1dfbcd80b377d455e649ef
Osmar Joseph <osmarjoseph@MacBook-Pro-de-Osmar.local>
2021-03-30 20:48:18 -0300

    refactor: move swagger config to better folder

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,6 @@
 import { setupMiddlewares } from './middlewares'
 import { setupRoutes } from './routes'
-import { setupSwagger } from './swagger'
+import { setupSwagger } from './swagger/swagger'
 import { setupApolloServer } from './apollo-server'
 
 import express from 'express'

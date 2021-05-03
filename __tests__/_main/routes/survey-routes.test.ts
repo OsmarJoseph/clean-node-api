fa654814c1c3320483d75fa2e326cb2d29484565
Osmar Joseph <osmarjoseph2013@hotmail.com>
2021-05-02 21:20:14 -0300

    chore: update prettier and eslint

--- __tests__/_main/routes/survey-routes.test.ts
+++ __tests__/_main/routes/survey-routes.test.ts
@@ -1,18 +1,23 @@
 import { app } from '@/main/config/app'
 import { mockAddSurveyParams } from '@/tests/_domain/mocks'
 import { MongoHelper } from '@/infra/db/mongodb/helpers'
-import { getAccountsCollection, AccountsCollection, getSurveysCollection, SurveysCollection } from '@/infra/db/mongodb/collections'
+import {
+  getAccountsCollection,
+  AccountsCollection,
+  getSurveysCollection,
+  SurveysCollection,
+} from '@/infra/db/mongodb/collections'
 import {
   addValidAccessToAccount,
   insertMockAccountOnDatabase,
-  insertMockSurveyOnDatabase
+  insertMockSurveyOnDatabase,
 } from '@/tests/_infra/mocks'
 import request from 'supertest'
 
 let surveysCollection: SurveysCollection
 let accountsCollection: AccountsCollection
 
-describe('Survey Routes',() => {
+describe('Survey Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
@@ -27,74 +32,75 @@ describe('Survey Routes',() => {
     accountsCollection = await getAccountsCollection()
     await accountsCollection.deleteMany({})
   })
-  describe('POST /surveys',() => {
-    test('Should return 403 on add survey without accessToken',async () => {
-      await request(app).post('/api/surveys')
-        .send(
-          mockAddSurveyParams()
-        )
-        .expect(403)
+  describe('POST /surveys', () => {
+    test('Should return 403 on add survey without accessToken', async () => {
+      await request(app).post('/api/surveys').send(mockAddSurveyParams()).expect(403)
     })
-    test('Should return 204 with valid accessToken',async () => {
+    test('Should return 204 with valid accessToken', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection,withAdminRole: true })
-      await request(app).post('/api/surveys').set('x-access-token',usedAccessToken)
-        .send(
-          mockAddSurveyParams()
-        )
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+        withAdminRole: true,
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', usedAccessToken)
+        .send(mockAddSurveyParams())
         .expect(204)
     })
-    test('Should return 403 with valid accessToken and no admin role',async () => {
+    test('Should return 403 with valid accessToken and no admin role', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection })
-      await request(app).post('/api/surveys').set('x-access-token',usedAccessToken)
-        .send(
-          mockAddSurveyParams()
-        )
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+      })
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', usedAccessToken)
+        .send(mockAddSurveyParams())
         .expect(403)
     })
   })
-  describe('GET /surveys',() => {
-    test('Should return 403 on load survey without accessToken',async () => {
+  describe('GET /surveys', () => {
+    test('Should return 403 on load survey without accessToken', async () => {
       await request(app).get('/api/surveys').expect(403)
     })
-    test('Should return 204 on load survey with accessToken and admin role no surveys saved',async () => {
+    test('Should return 204 on load survey with accessToken and admin role no surveys saved', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection,withAdminRole: true })
-      await
-      request(app)
-        .get('/api/surveys')
-        .set('x-access-token',usedAccessToken)
-        .expect(204)
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+        withAdminRole: true,
+      })
+      await request(app).get('/api/surveys').set('x-access-token', usedAccessToken).expect(204)
     })
-    test('Should return 204 on load survey with accessToken and no role and no surveys saved',async () => {
+    test('Should return 204 on load survey with accessToken and no role and no surveys saved', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection })
-      await
-      request(app)
-        .get('/api/surveys')
-        .set('x-access-token',usedAccessToken)
-        .expect(204)
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+      })
+      await request(app).get('/api/surveys').set('x-access-token', usedAccessToken).expect(204)
     })
-    test('Should return 200 on load survey with accessToken and admin role with surveys saved',async () => {
+    test('Should return 200 on load survey with accessToken and admin role with surveys saved', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection, withAdminRole: true })
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+        withAdminRole: true,
+      })
       await insertMockSurveyOnDatabase(surveysCollection)
-      await
-      request(app)
-        .get('/api/surveys')
-        .set('x-access-token',usedAccessToken)
-        .expect(200)
+      await request(app).get('/api/surveys').set('x-access-token', usedAccessToken).expect(200)
     })
-    test('Should return 200 on load survey with accessToken and no role with surveys saved',async () => {
+    test('Should return 200 on load survey with accessToken and no role with surveys saved', async () => {
       const mockAccount = await insertMockAccountOnDatabase(accountsCollection)
-      const usedAccessToken = await addValidAccessToAccount({ account: mockAccount,accountsCollection })
+      const usedAccessToken = await addValidAccessToAccount({
+        account: mockAccount,
+        accountsCollection,
+      })
       await insertMockSurveyOnDatabase(surveysCollection)
-      await
-      request(app)
-        .get('/api/surveys')
-        .set('x-access-token',usedAccessToken)
-        .expect(200)
+      await request(app).get('/api/surveys').set('x-access-token', usedAccessToken).expect(200)
     })
   })
 })
--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -1,83 +1,91 @@
-export const surveyResultPath = {
+export const surveyResultPath: unknown = {
   put: {
     tags: ['Enquete'],
     summary: 'API para criar a resposta de uma enquete',
-    security: [{
-      apiKeyAuthSchema: []
-    }],
+    security: [
+      {
+        apiKeyAuthSchema: [],
+      },
+    ],
     requestBody: {
       content: {
         'application/json': {
           schema: {
-            $ref: '#/schemas/saveSurveyResultParamsSchema'
-          }
-        }
-      }
+            $ref: '#/schemas/saveSurveyResultParamsSchema',
+          },
+        },
+      },
     },
-    parameters: [{
-      in: 'path',
-      name: 'surveyId',
-      required: true,
-      schema: {
-        type: 'string'
-      }
-    }],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
         content: {
           'application/json': {
             schema: {
-              $ref: '#/schemas/surveyResultSchema'
-            }
-          }
-        }
+              $ref: '#/schemas/surveyResultSchema',
+            },
+          },
+        },
       },
       403: {
-        $ref: '#/components/forbidenRequestComponent'
+        $ref: '#/components/forbidenRequestComponent',
       },
       404: {
-        $ref: '#/components/notFoundComponent'
+        $ref: '#/components/notFoundComponent',
       },
       500: {
-        $ref: '#/components/serverErrorComponent'
-      }
-    }
+        $ref: '#/components/serverErrorComponent',
+      },
+    },
   },
   get: {
     tags: ['Enquete'],
     summary: 'API para consultar o resultado de uma enquete',
-    security: [{
-      apiKeyAuthSchema: []
-    }],
-    parameters: [{
-      in: 'path',
-      name: 'surveyId',
-      required: true,
-      schema: {
-        type: 'string'
-      }
-    }],
+    security: [
+      {
+        apiKeyAuthSchema: [],
+      },
+    ],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string',
+        },
+      },
+    ],
     responses: {
       200: {
         description: 'Sucesso',
         content: {
           'application/json': {
             schema: {
-              $ref: '#/schemas/surveyResultSchema'
-            }
-          }
-        }
+              $ref: '#/schemas/surveyResultSchema',
+            },
+          },
+        },
       },
       403: {
-        $ref: '#/components/forbidenRequestComponent'
+        $ref: '#/components/forbidenRequestComponent',
       },
       404: {
-        $ref: '#/components/notFoundComponent'
+        $ref: '#/components/notFoundComponent',
       },
       500: {
-        $ref: '#/components/serverErrorComponent'
-      }
-    }
-  }
+        $ref: '#/components/serverErrorComponent',
+      },
+    },
+  },
 }
